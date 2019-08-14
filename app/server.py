7de57c5d3c095c5325bb2ae705e7d4ec91c013c5
Kalyan Aravapalli <31206412+kcaravapalli@users.noreply.github.com>
2019-08-14 14:56:06 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1QTpstET8_Tn9zXWnYrDM3wwFPVQZoHDQ'
+export_file_url = 'https://www.dropbox.com/s/zstxsfvyylm8eat/blindness-detection-ResNet50-Clahe.pkl?dl=1'
 #export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
 export_file_name = 'export.pkl'
 
